4713109b43c17f0f50f0c5a6bef92b05cff567b2
FedeQuarta <quartafederico996@gmail.com>
2024-04-04 02:39:37 +0200

    Aggiunta tasti ultimi per gioca storia

---
+++ src/main/java/com/example/client/Pair.java
@@ -0,0 +1,41 @@
+package com.example.client;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Pair<A, B> implements Serializable {
+    private A a;
+    private B b;
+
+    public Pair(final A a, final B b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public Pair() {
+    }
+
+    public A getA() {
+        return a;
+    }
+
+    public B getB() {
+        return b;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(a, pair.a) && Objects.equals(b, pair.b);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
+    }
+
+}
